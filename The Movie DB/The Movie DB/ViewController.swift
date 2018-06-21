008ccbac76ae1653c06de56e8ac4c44fff33f788
Mohamed Shaban <mohamed461994@gmail.com>
2018-06-22 01:51:50 +0200

    feat: intial xcode project

---
+++ The Movie DB/The Movie DB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Movie DB
+//
+//  Created by Mohamed Shaban on 6/22/18.
+//  Copyright © 2018 Mohammed Shaban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
